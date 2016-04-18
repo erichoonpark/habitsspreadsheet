ec87c470288ae2ffc2ee6bfc2c3028047a644e16
Eric Park <erichoonpark@gmail.com>
2016-04-18 13:12:16 -0700

    Routine-CREATE

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import AccountsUIWrapper from './AccountsUIWrapper.jsx';
-
 import { Routines } from '../api/routines.js';
-
 import Routine from './Routine.jsx'
 
 class App extends Component {
+	handleSubmit(event) {
+    event.preventDefault();
+ 
+    // Find the text field via the React ref
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+ 
+    Routines.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+ 
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.textInput).value = '';
+  }
+
+
 	renderRoutines(){
 		return this.props.routines.map((routine) => (
       		<Routine key={routine._id} routine={routine} />
@@ -19,9 +33,17 @@ class App extends Component {
 		return (
 			<div>
 				<AccountsUIWrapper />
+				<h1>Routines:</h1>
 				<ul>
           			{this.renderRoutines()}
         		</ul>
+        		<form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new routines"
+            />
+          </form>
         	</div>
 		)
 	}
